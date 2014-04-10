325a1a9d460e038411bd29dd4ab29f4237d05bb8
Joe Barber <joemarxbarber@gmail.com>
2014-04-10 02:45:37 -0500

    GoPro animation upload

---
+++ GoPro/Portrait Selector_edgeActions.js
@@ -0,0 +1,50 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Rectangle2}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Resolution2}","click",function(sym,e){var mySymbolObject=sym.getSymbol("Resolution2").play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'mode'
+(function(symbolName){})("mode");
+//Edge symbol end:'mode'
+
+//=========================================================
+
+//Edge symbol: 'Resolution'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3000,function(sym,e){sym.playReverse();});
+//Edge binding end
+})("Resolution");
+//Edge symbol end:'Resolution'
+
+//=========================================================
+
+//Edge symbol: 'Framerate'
+(function(symbolName){})("Framerate");
+//Edge symbol end:'Framerate'
+
+//=========================================================
+
+//Edge symbol: 'Fov'
+(function(symbolName){})("Fov");
+//Edge symbol end:'Fov'
+
+//=========================================================
+
+//Edge symbol: 'protune'
+(function(symbolName){})("protune");
+//Edge symbol end:'protune'
+
+//=========================================================
+
+//Edge symbol: 'settings'
+(function(symbolName){})("settings");
+//Edge symbol end:'settings'
+})(jQuery,AdobeEdge,"EDGE-264160235");
